74d58386c431994ded35af3cb1df4e1a09299c2c
Pedro Barreto <pedro.barreto.ne@gmail.com>
2023-09-18 17:08:52 -0300

    finalizando projeto

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,3 +1,4 @@
+import { ConsultaCepService } from './../service/consulta-cep.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
@@ -9,9 +10,32 @@ import { NgForm } from '@angular/forms';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private ConsultaCepService: ConsultaCepService
+    ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  consultaCEP(ev: any, f: NgForm){
+    const cep = ev.target.value;
+    if(cep !== ''){
+      this.ConsultaCepService.getConsultaCep(cep).subscribe(resultado => {
+        console.log(resultado)
+        this.populandoEndereco(resultado, f);
+      });
+    }
+  }
+
+  populandoEndereco(dados: any, f: NgForm){
+    f.form.patchValue({
+      endereco: dados.logradouro,
+      complemento: dados.complemento,
+      bairro: dados.bairro,
+      cidade: dados.localidade,
+      estado: dados.uf
+    })
+    console.log(dados.bairro)
   }
 
   cadastrar(form: NgForm){
@@ -20,6 +44,6 @@ export class CadastroComponent implements OnInit {
     } else {
       alert("Formulário Invalido!")
     }
-      console.log(form);
+      console.log(form.controls);
   }
 }
